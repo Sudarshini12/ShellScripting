d89407c428bb68e9a9466a27638b929c1a24a03b
Sudarshini12 <107766419+Sudarshini12@users.noreply.github.com>
2024-06-26 21:46:00 +0530

    Update deletelog.sh

--- deletelog.sh
+++ deletelog.sh
@@ -35,3 +35,22 @@ if [ $file_count -gt 0 ]; then
 else
     echo "There are no files older than 30 days."
 fi
+
+
+
+
+#!/bin/bash
+echo "This script deletes files older than 30 days."
+path="$1"
+echo "Checking path: $path"
+
+# Check if there are any files older than 30 days
+file_count=$(find "$path" -type f -mtime +30 | wc -l)
+
+if [ $file_count -eq 0 ]; then
+    echo "There are no files older than 30 days."
+elif find "$path" -type f -mtime +30 -delete; then
+    echo "Files older than 30 days were successfully deleted."
+else
+    echo "There was an issue deleting the files."
+fi
