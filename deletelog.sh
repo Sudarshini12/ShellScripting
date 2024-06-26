7f7768af8e27497c3015b5ee56a6311042349739
Sudarshini12 <107766419+Sudarshini12@users.noreply.github.com>
2024-06-26 20:34:08 +0530

    Update deletelog.sh
    checking if there are any files older than 30 if no files found display a msg there are no files to delete

--- deletelog.sh
+++ deletelog.sh
@@ -9,3 +9,30 @@ then
 else
    echo "deleteion was having some issue"
 fi
+
+
+
+
+
+
+
+
+#!/bin/bash
+echo "This script deletes files older than 30 days."
+path="$1"
+echo "Checking path: $path"
+
+# Check if there are any files older than 30 days
+file_count=$(find "$path" -type f -mtime +30 | wc -l)
+
+if [ $file_count -gt 0 ]; then
+    # Delete the files older than 30 days
+    find "$path" -type f -mtime +30 -delete
+    if [[ $? -eq 0 ]]; then
+        echo "Files older than 30 days were successfully deleted."
+    else
+        echo "There was an issue deleting the files."
+    fi
+else
+    echo "There are no files older than 30 days."
+fi
